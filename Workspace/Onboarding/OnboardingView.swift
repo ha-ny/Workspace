0bac079738dc8386f8f90be5cc44717a224c778a
ha-ny <hany01.dev@gmail.com>
2024-01-02 14:47:58 +0900

    LaunchScreen 디자인

---
+++ Workspace/Onboarding/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Workspace
+//
+//  Created by 김하은 on 1/2/24.
+//
+
+import UIKit
+
+class OnboardingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
