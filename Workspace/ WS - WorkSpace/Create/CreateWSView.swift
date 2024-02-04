841bd6984cc3815cbc9b4ef3b86ae852f9889db0
ha-ny <hany01.dev@gmail.com>
2024-02-04 21:14:45 +0900

    워크스페이스 디자인

---
+++ Workspace/ WS - WorkSpace/Create/CreateWSView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateWSView.swift
+//  Workspace
+//
+//  Created by 김하은 on 1/19/24.
+//
+
+import UIKit
+
+class CreateWSView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
