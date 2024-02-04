ac84afa5b86484b0d688abf66becb6db36fabc64
ha-ny <hany01.dev@gmail.com>
2024-02-04 21:38:56 +0900

    파일 수정

--- Workspace/ WS - WorkSpace/Create/CreateWSView.swift
+++ Workspace/ WS - WorkSpace/Create/CreateWSView.swift
@@ -7,14 +7,7 @@
 
 import UIKit
 
-class CreateWSView: UIView {
+class CreateWSView: BaseView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    
 }
--- Workspace/ WS - WorkSpace/Create/CreateWSViewController.swift
+++ Workspace/ WS - WorkSpace/Create/CreateWSViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 final class CreateWSController: UIViewController {
 
+    private let mainView = CreateWSView()
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
     }
 }
